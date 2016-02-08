0aa1e7d934b62d3ba8b4b9e46ef9525d185f4348
Alexis Aguilar <aguilar.alexis01@gmail.com>
2016-02-08 16:01:55 -0600

    Refactorizando codigo

--- tmpl/noticias.php
+++ tmpl/noticias.php
@@ -3,45 +3,44 @@
 defined('_JEXEC') or die('Restricted access');
 $document->addStyleSheet(JURI::root(true) . '/modules/' . $module->module . '/assets/noticias/jquery.booklet.latest.css');
 $document->addScript(JURI::root(true) . '/modules/' . $module->module . '/assets/noticias/jquery.booklet.latest.min.js');
-$auto=0;
-if ($params->get('autoplay')==0)
-{
-    $auto='false';
-}else
-{
-    $auto='true';
+$auto = 0;
+if ($params->get('autoplay') == 0) {
+    $auto = 'false';
+} else {
+    $auto = 'true';
 }
 ?>
 
 
 <div id="mybook">
-    
-    <?php foreach ($slides as $slide) { ?>
-    <div class="hoja" title="<?php echo $slide->title; ?>">
 
-        <div class="titulo-nota">
-            <h3><?php if($params->get('show_title')) {echo $slide->title;} ?></h3>
-        </div>
-        <div class="imagen-nota">
-            <img src="<?php echo JURI::root().$slide->image; ?>" alt="<?php echo $slide->title; ?>">
-        </div>
-        <div class="contenido-nota">
-            
-           <?php 
-            if($params->get('show_desc'))
-            {
-           echo $slide->description; 
-            }
-           ?>
+<?php foreach ($slides as $slide) { ?>
+        <div class="hoja" title="<?php echo $slide->title; ?>">
+
+            <div class="titulo-nota">
+                <h3><?php if ($params->get('show_title')) {
+        echo $slide->title;
+    } ?></h3>
+            </div>
+            <div class="imagen-nota">
+                <img src="<?php echo JURI::root() . $slide->image; ?>" alt="<?php echo $slide->title; ?>">
+            </div>
+            <div class="contenido-nota">
+
+                <?php
+                if ($params->get('show_desc')) {
+                    echo $slide->description;
+                }
+                ?>
+
+            </div>
+            <div class="pie">
+                <hr>
+                <a href="#">Leer mas</a>
+            </div>
 
         </div>
-        <div class="pie">
-            <hr>
-            <a href="#">Leer mas</a>
-        </div>
-
-    </div>
-    <?php  }  ?>
+    <?php } ?>
 
 
 </div>
@@ -49,16 +48,6 @@ if ($params->get('autoplay')==0)
 
 <script>
     jQuery(function () {
-        jQuery("#mybook").booklet({
-            width: '100%',
-            auto:<?php echo $auto; ?>,
-            tabs: true,
-            tabWidth: 180,
-            tabHeight: 20,
-            nextControlText: "Siguiente",
-            previousControlText: "Anterior",
-            height: 650
-        });
-
+        jQuery("#mybook").booklet({width: "100%", auto:<?php echo $auto; ?>, tabs: !0, tabWidth: 180, tabHeight: 20, nextControlText: "Siguiente", previousControlText: "Anterior", height: 650})
     });
 </script>
--- tmpl/principal.php
+++ tmpl/principal.php
@@ -1,40 +1,12 @@
 <?php
 // no direct access
 defined('_JEXEC') or die('Restricted access');
-
 $document->addStyleSheet(JURI::root(true) . '/modules/' . $module->module . '/assets/principal/boutique.css');
 $document->addScript(JURI::root(true) . '/modules/' . $module->module . '/assets/principal/jquery.boutique.min.js');
 ?>
 
 <script>
-    // Calling Boutique on your HTML element when the document is ready:
-    jQuery(document).ready(function () {
-
-        // Generate bullets
-        jQuery.each(jQuery('#boutique').children('li'), function () {
-            jQuery('#bullets').append('<div class="bullet" />');
-        });
-        jQuery('.bullet').first().addClass('active');
-
-        // Bullet click event
-        jQuery('.bullet').click(function () {
-            var index = jQuery(this).index();
-            boutique_goto(index + 1);
-        });
-
-        // Initiate Boutique
-        jQuery('#boutique').boutique({
-            container_width: 'auto',
-            autoplay:true,
-            front_img_width: 700,
-            front_img_height: 380
-        });
-    });
-    // Change the active bullet to match current frame
-    var pre_move_callback = function (href, containerid, framenumber) {
-        jQuery('#bullets').children('.active').removeClass('active');
-        jQuery('.bullet').eq(framenumber - 1).addClass('active');
-    }
+jQuery(document).ready(function(){jQuery.each(jQuery("#boutique").children("li"),function(){jQuery("#bullets").append('<div class="bullet" />')}),jQuery(".bullet").first().addClass("active"),jQuery(".bullet").click(function(){var e=jQuery(this).index();boutique_goto(e+1)}),jQuery("#boutique").boutique({container_width:"auto",autoplay:!0,front_img_width:700,front_img_height:380})});var pre_move_callback=function(e,u,t){jQuery("#bullets").children(".active").removeClass("active"),jQuery(".bullet").eq(t-1).addClass("active")};
 </script>
 
 <div id="contenidoSlider">
@@ -55,9 +27,4 @@ $document->addScript(JURI::root(true) . '/modules/' . $module->module . '/assets
     </ul>
     <div id="bullets"></div>
     <!-- End of the Boutique HTML -->
-</div>
-
-
-<div class="camera_wrap camera_azure_skin" id="camera_wrap_1">
-
 </div>
